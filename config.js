d8d3f307693f8a85b1c8dd5e8e9dbcb45bcafaf6
Nasir1244 <157060846+Nasir1244@users.noreply.github.com>
2024-01-19 09:01:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923023594323
+', 'Nasir1244', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
